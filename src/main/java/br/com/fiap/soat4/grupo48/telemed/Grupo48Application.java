3b00b5edd5df6eef7d690b8c2ee7a00c18aee480
André Felipe Laus <andre.felipe.laus@gmail.com>
2024-07-19 21:56:49 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/fiap/soat4/grupo48/telemed/Grupo48Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat4.grupo48.telemed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo48Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo48Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/soat4/grupo48/telemed/Grupo48ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat4.grupo48.telemed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo48ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
