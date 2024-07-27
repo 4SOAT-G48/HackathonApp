1e1ed20ca3447a0e41b36bc6ceb865c1c900b597
André Felipe Laus <andre.felipe.laus@gmail.com>
2024-07-27 16:37:50 -0300

    documentação da API

--- src/test/java/br/com/fiap/soat4/grupo48/telemed/Grupo48ApplicationTests.java
+++ src/test/java/br/com/fiap/soat4/grupo48/telemed/Grupo48ApplicationTests.java
@@ -1,13 +1,23 @@
 package br.com.fiap.soat4.grupo48.telemed;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Grupo48ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    void applicationStartsSuccessfully() {
+        Grupo48Application.main(new String[]{});
+        // Assuming the application should start and be accessible
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertEquals("Expected response", body);
+    }
 }
